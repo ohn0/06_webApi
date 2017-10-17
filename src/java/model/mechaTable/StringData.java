ec2dde979b71b83ba5e91114683e5dafc1a40411
ohn0 <silverskinx@gmail.com>
2017-10-17 10:57:10 -0400

    added files

---
+++ src/java/model/mechaTable/StringData.java
@@ -0,0 +1,9 @@
+package model.mechaTable;
+
+public class StringData {
+
+    // select country_id, country_name, flag_abbrev, flag_URL from country_flag
+    public String mechaTable_ID = "";
+    public String mechaDescriptor = "";
+    public String errorMsg = "";
+}
---
+++ src/java/model/mechaTable/StringDataList.java
@@ -0,0 +1,49 @@
+package model.mechaTable;
+
+import dbUtils.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class StringDataList {
+
+    public String dbError = "";
+    private ArrayList<StringData> recordList = new ArrayList();
+
+    // Default constructor just leaves the 2 data members initialized as above
+    public StringDataList() {
+    }
+
+    // overloaded constructor populates the list (and possibly the dbError)
+    public StringDataList(String mechaNameStartsWith, DbConn dbc) {
+
+        StringData sd = new StringData();
+
+        System.out.println("Searching for mecha that start with " + mechaNameStartsWith);
+
+        try {
+
+            String sql = "SELECT mechaTable_ID, mechaName FROM mechaTable "
+                    + "WHERE mechaName LIKE ? ORDER BY mechaTable_ID";
+
+            PreparedStatement stmt = dbc.getConn().prepareStatement(sql);
+            stmt.setString(1, mechaNameStartsWith + "%");
+            ResultSet results = stmt.executeQuery();
+
+            while (results.next()) {
+                try {
+                    sd = new StringData();
+                    sd.mechaTable_ID = FormatUtils.formatInteger(results.getObject("mechaTable_ID"));
+                    sd.mechaDescriptor = FormatUtils.formatString(results.getObject("mechaName"));
+                    this.recordList.add(sd);
+                } catch (Exception e) {
+                    sd.errorMsg = "Something's wrong " + e.getMessage();
+                    this.recordList.add(sd);
+                }
+            } // while
+        } catch (Exception e) {
+            this.dbError = "Somethign else is wrong " + e.getMessage();
+        }
+    } // method
+
+} // class
